d575941df7555a2abde3e18e5130d3cab1219556
Dairon Canel <98697567+dairon-canel@users.noreply.github.com>
2023-03-27 02:07:15 -0400

    feat: integrate back

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api':
-        'https://api-rest-ts-node-gateways-master-production.up.railway.app',
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
+  // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
+
+  return defineConfig({
+    server: {
+      proxy: {
+        port: process.env.VITE_PORT,
+        '/api': process.env.VITE_DOMAIN,
+      },
     },
-  },
-  plugins: [react()],
-});
+    plugins: [react()],
+  });
+};

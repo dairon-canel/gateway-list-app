793b78a7bb8dd11904020e3f2491b9717183f131
Dairon Canel <98697567+dairon-canel@users.noreply.github.com>
2023-03-27 02:21:35 -0400

    feat: trying t oconnect with bkend

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,11 @@ import react from '@vitejs/plugin-react';
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-  // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
-  // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
-
   return defineConfig({
     server: {
       proxy: {
-        port: process.env.VITE_PORT,
-        '/api': process.env.VITE_DOMAIN,
+        port: process.env.VITE_VERCEL_PORT,
+        '/api': process.env.VITE_VERCEL_DOMAIN,
       },
     },
     plugins: [react()],
